fb7ddec14a64cf825b57d2f3f63f91ee496813dd
CiubancanCristian <cristian@ciubancan.com>
2019-08-02 18:00:27 +0300

    menu and cart close on logo click

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,6 +8,8 @@ import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 import MenuIcon from "../menu-icon/menu-icon.component";
 
+import { toggleCartOff } from "../../redux/cart/cart.actions";
+import { toggleMenuOff } from "../../redux/menu/menu.actions";
 import Navigation from "../navigation-container/navigation-container.component";
 import { ReactComponent as Farrod } from "../../assets/farrod.svg";
 import CartDropdown from "../cart-dropdown/cart-dropdown.component";
@@ -18,51 +20,67 @@ import Magnifier from "./magnifier.png";
 
 import "./header.styles.scss";
 
-const Header = ({ currentUser, isVisible, hidden }) => (
-  <div className="">
-    <div
-      className={classNames("header", {
-        "header--hidden": !isVisible
-      })}
-    >
-      <div className="navig">
-        <Navigation />
-      </div>
-      <div className="menu-button ">
-        <MenuIcon />
-      </div>
-      <div className="options">
-        <Link className="option" to="/shop">
-          SHOP
-        </Link>
-        <Link className="option" to="/shop">
-          CONTACT
-        </Link>
-        {currentUser ? (
-          <div className="option" onClick={() => auth.signOut()}>
-            SIGN OUT
-          </div>
-        ) : (
-          <Link className="option" to="/signin">
-            SIGN IN
+const Header = ({
+  currentUser,
+  isVisible,
+  hidden,
+  toggleMenuOff,
+  toggleCartOff
+}) => {
+  const onLogoClick = () => {
+    toggleCartOff();
+    toggleMenuOff();
+  };
+  return (
+    <div className="">
+      <div
+        className={classNames("header", {
+          "header--hidden": !isVisible
+        })}
+      >
+        <div className="navig">
+          <Navigation />
+        </div>
+        <div className="menu-button ">
+          <MenuIcon />
+        </div>
+        <div className="options">
+          <Link className="option" to="/shop">
+            SHOP
           </Link>
-        )}
+          <Link className="option" to="/shop">
+            CONTACT
+          </Link>
+          {currentUser ? (
+            <div className="option" onClick={() => auth.signOut()}>
+              SIGN OUT
+            </div>
+          ) : (
+            <Link className="option" to="/signin">
+              SIGN IN
+            </Link>
+          )}
+        </div>
+        <Link className="logo-container" onClick={onLogoClick} to="/">
+          <Farrod className="logo" />
+        </Link>
+        <div className="search">
+          <img className="search-item lens" alt="search" src={Magnifier} />
+          <CartIcon className="search-item" />
+        </div>
+        {hidden ? null : <CartDropdown />}
       </div>
-      <Link className="logo-container" to="/">
-        <Farrod className="logo" />
-      </Link>
-      <div className="search">
-        <img className="search-item lens" alt="search" src={Magnifier} />
-        <CartIcon className="search-item" />
-      </div>
-      {hidden ? null : <CartDropdown />}
     </div>
-  </div>
-);
+  );
+};
+const mapDispatchToProps = dispatch => ({
+  toggleCartOff: () => dispatch(toggleCartOff()),
+  toggleMenuOff: () => dispatch(toggleMenuOff())
+});
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
   hidden: selectCartHidden
 });
 
-export default connect(mapStateToProps)(Header);
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
